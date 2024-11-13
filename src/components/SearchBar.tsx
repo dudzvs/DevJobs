808d6c5ad32fad75f7621888b95910f3c6415e18
dudz <eduardo_vteixeira@outlook.com>
2024-11-12 21:02:28 -0300

    feat: add mobile filter form toggle
    - Added `useState` to toggle `showFilterForm` for mobile filter form display.
    - Updated layout and styles for responsiveness on mobile.
    - Adjusted element positioning and visibility based on screen size.
    - Revised button styles to improve mobile user experience.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,14 @@
 import searchIcon from '../../public/images/desktop/icon-search.svg';
 import locationIcon from '../../public/images/desktop/icon-location.svg';
 import filterIcon from '../../public/images/mobile/icon-filter.svg';
+import { useState } from 'react';
 
 const SearchBar = () => {
+	const [showFilterForm, setShowFilterForm] = useState(false);
+
 	return (
 		<>
-			<form className="flex dark:text-amber-50 bg-primary-bgMains dark:bg-primary-secondary rounded-[6px] justify-center items-center p-4 pt-2 pb-2 shadow-2xl sm:gap-2">
+			<form className="flex dark:text-amber-50 bg-primary-bgMains dark:bg-primary-secondary rounded-[6px] ml-4 mr-4 justify-center items-center p-4 pt-2 pb-2 shadow-2xl sm:gap-2">
 				<label htmlFor="filterByTitle">
 					<img className="mr-2.5" src={searchIcon} />
 				</label>
@@ -16,36 +19,47 @@ const SearchBar = () => {
 					name="filterByTitle"
 					placeholder="Filter by title..."
 				/>
-				<label htmlFor="filterLocation">
-					<img
-						className="mr-2.5 hidden sm:block"
-						src={locationIcon}
-					/>
-				</label>
-				<input
-					className="h-12 outline-none dark:bg-primary-secondary hidden sm:block"
-					type="text"
-					id="filterLocation"
-					name="filterLocation"
-					placeholder="Filter by location..."
-				/>
-				<input
-					type="checkbox"
-					id="fullTimeJobs"
-					name="fullTimeJobs"
-					className="hidden sm:block flex-shrink-0 dark:bg-primary-secondary appearance-none w-5 h-5 bg-primary-tertiary grayscale brightness-150 opacity-80 cursor-pointer rounded checked:bg-iconChecked bg-no-repeat bg-center bg-contain"
-				/>
-				<label
-					className="font-bold text-sm flex-shrink-0  hidden sm:block"
-					htmlFor="fullTimeJobs"
+				<div
+					className={`gap-4 shadow-2xl rounded absolute top-56 flex ${!showFilterForm ? 'invisible' : ''} pt-6 pb-6 pl-12 pr-12 flex-col sm:items-center sm:flex sm:flex-row sm:relative sm:top-0 sm:shadow-none sm:visible`}
 				>
-					Full Time Only
-				</label>
+					<label className="relative" htmlFor="filterLocation">
+						<img
+							className="mr-2.5 absolute top-4 left-[-30px] sm:block sm:relative sm:top-0 sm:left-0"
+							src={locationIcon}
+						/>
+					</label>
+					<input
+						className="h-6 outline-none dark:bg-primary-secondary "
+						type="text"
+						id="filterLocation"
+						name="filterLocation"
+						placeholder="Filter by location..."
+					/>
+					<div className="min-w-full border-b-2 block sm:hidden bg-gray-50"></div>
+					<input
+						type="checkbox"
+						id="fullTimeJobs"
+						name="fullTimeJobs"
+						className=" absolute bottom-24 left-4 sm:relative sm:left-0 sm:bottom-0 flex-shrink-0 dark:bg-primary-secondary appearance-none w-5 h-5 bg-primary-tertiary grayscale brightness-150 opacity-80 cursor-pointer rounded checked:bg-iconChecked bg-no-repeat bg-center bg-contain"
+					/>
+					<label
+						className="text-sm flex-shrink-0 font-bold "
+						htmlFor="fullTimeJobs"
+					>
+						Full Time Only
+					</label>
 
-				<button type="button" className="sm:hidden block">
+					<button className="block sm:hidden text-amber-50 rounded p-2 bg-primary-main w-full self-center mt-4">
+						Search
+					</button>
+				</div>
+				<button
+					type="button"
+					onClick={() => setShowFilterForm(!showFilterForm)}
+					className=" sm:hidden block"
+				>
 					<img src={filterIcon} className="mr-3.5" />
 				</button>
-
 				<button
 					type="submit"
 					className=" flex justify-center items-center bg-primary-main sm:ml-3 sm:pl-4 sm:pr-4 sm:pt-2 sm:pb-2 p-3 pl-3 pr-3 text-amber-50 font-bold rounded text-transparent sm:text-amber-50"
@@ -53,7 +67,7 @@ const SearchBar = () => {
 					<span className="hidden sm:inline">Search</span>
 					<img
 						src={searchIcon}
-						className="sm:hidden  w-5 h-5 filter invert brightness-0"
+						className="sm:hidden w-5 h-5 filter invert brightness-0"
 					/>
 				</button>
 			</form>
